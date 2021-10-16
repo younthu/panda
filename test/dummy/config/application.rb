9770fd7340960192641e0905a55acb2b2cd31f1f
Zhiyong Yang <evil_eve_live@outlook.com>
2021-10-16 15:32:48 +0800

    localization

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -18,5 +18,8 @@ module Dummy
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en,:'zh-CN']
+    config.i18n.default_locale = :'zh-CN'
+    config.time_zone = "Asia/Shanghai"
   end
 end
