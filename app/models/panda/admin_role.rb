65cff984612081fb06b1c7eae55c48f06d0c1bb7
Zhiyong Yang <evil_eve_live@outlook.com>
2021-10-24 15:59:44 +0800

    missing parts for rolify. The rolify generator does not work well with engine.

--- app/models/panda/admin_role.rb
+++ app/models/panda/admin_role.rb
@@ -1,4 +1,16 @@
 module Panda
   class AdminRole < ApplicationRecord
+    has_and_belongs_to_many :admin_users, :join_table => :panda_admin_users_panda_admin_roles
+
+    belongs_to :resource,
+               :polymorphic => true,
+               :optional => true
+
+
+    validates :resource_type,
+              :inclusion => { :in => Rolify.resource_types },
+              :allow_nil => true
+
+    scopify
   end
 end
