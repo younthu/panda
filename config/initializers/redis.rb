c7f48dc64b119eb0029969153494d91bd5b50906
Andrew Yang <evil_eve_live@outlook.com>
2023-09-02 11:23:41 +0800

    Update redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 redis_params = {
-  host: Settings.redis.host,
-  port: Settings.redis.port,
+  host: Settings.redis&.host,
+  port: Settings.redis&.port,
   id: nil
 }
-redis_params.merge!(password: Settings.redis.password) if Settings.redis.password
+redis_params.merge!(password: Settings.redis&.password) if Settings.redis&.password
 
 sidekiq_redis_params = {
-  url: "redis://#{Settings.redis.host}:#{Settings.redis.port}/#{Settings.redis.sidekiq_db}",
-  namespace: Settings.redis.namespace
+  url: "redis://#{Settings.redis&.host}:#{Settings.redis&.port}/#{Settings.redis&.sidekiq_db}",
+  namespace: Settings.redis&.namespace
 }
-sidekiq_redis_params.merge!(password: Settings.redis.password) if Settings.redis.password
+sidekiq_redis_params.merge!(password: Settings.redis&.password) if Settings.redis&.password
 
 $redis = Redis::Namespace.new(
-  Settings.redis.namespace,
-  redis: Redis.new(redis_params.merge(db: Settings.redis.queue))
+  Settings.redis&.namespace,
+  redis: Redis.new(redis_params.merge(db: Settings.redis&.queue))
 )
 
 Sidekiq.configure_server do |config|
