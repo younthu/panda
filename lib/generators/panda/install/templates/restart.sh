6b7fc3f00044c97333060b00ce0b70e8632e3450
Andrew Yang <evil_eve_live@outlook.com>
2023-09-02 12:26:37 +0800

    Update restart.sh

--- lib/generators/panda/install/templates/restart.sh
+++ lib/generators/panda/install/templates/restart.sh
@@ -1,22 +1,22 @@
 docker login --username=651xxx@qq.com --password=xxxxxx1212036 registry.cn-shanghai.aliyuncs.com
 
-docker pull registry.cn-shanghai.aliyuncs.com/younthu/<%=app_name%>:latest
+docker pull registry.cn-shanghai.aliyuncs.com/younthu/<%= config[:app_name] %>:latest
 
-docker run --rm -e RACK_ENV=production registry.cn-shanghai.aliyuncs.com/younthu/<%=app_name%>:latest bundle exec rake db:migrate
+docker run --rm -e RACK_ENV=production registry.cn-shanghai.aliyuncs.com/younthu/<%= config[:app_name] %>:latest bundle exec rake db:migrate
 
-docker rm -f <%=app_name%>_sidekiq
+docker rm -f <%= config[:app_name] %>_sidekiq
 
-docker run -d --restart=always --name <%=app_name%>_sidekiq registry.cn-shanghai.aliyuncs.com/younthu/<%=app_name%>:latest bundle exec sidekiq -C config/sidekiq.yml -e production
+docker run -d --restart=always --name <%= config[:app_name] %>_sidekiq registry.cn-shanghai.aliyuncs.com/younthu/<%= config[:app_name] %>:latest bundle exec sidekiq -C config/sidekiq.yml -e production
 
-docker rm -f <%=app_name%>
+docker rm -f <%= config[:app_name] %>
 
-docker run -d --restart=always -p 3000:3000 --name <%=app_name%> registry.cn-shanghai.aliyuncs.com/younthu/<%=app_name%>:latest bundle exec rails s -e production
+docker run -d --restart=always -p 3000:3000 --name <%= config[:app_name] %> registry.cn-shanghai.aliyuncs.com/younthu/<%= config[:app_name] %>:latest bundle exec rails s -e production
 
 docker image prune -f
 docker container prune -f
 
 docker ps
 
-docker network connect --alias <%=app_name%> <%=app_name%>-net <%=app_name%>
+docker network connect --alias <%= config[:app_name] %> <%= config[:app_name] %>-net <%= config[:app_name] %>
 
-docker logs -f <%=app_name%>
+docker logs -f <%=config[:app_name] %>
